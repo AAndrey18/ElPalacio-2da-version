040c4c140aa21c5165b5d91b1e9ab2ffe313de51
AAndrey18 <brauliobk.12@gmail.com>
2026-06-18 01:46:54 -0600

    a

--- mi-tienda-spa/src/config.js
+++
@@ -1,7 +0,0 @@
-const PRODUCTION_BACKEND_URL = ""; 
-
-const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-  ? 'http://localhost:3001' 
-  : PRODUCTION_BACKEND_URL; 
-
-export default API_BASE_URL;
