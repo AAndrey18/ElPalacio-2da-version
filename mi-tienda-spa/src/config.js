0492edb7a8fede5e122be60d7f5b90f6bbef988d
AAndrey18 <brauliobk.12@gmail.com>
2026-06-18 01:40:36 -0600

    cambio de url 3

--- mi-tienda-spa/src/config.js
+++ mi-tienda-spa/src/config.js
@@ -1,4 +1,4 @@
-const PRODUCTION_BACKEND_URL = "https://el-palacio-2da-version-pqtm.vercel.app/"; 
+const PRODUCTION_BACKEND_URL = ""; 
 
 const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://localhost:3001' 
